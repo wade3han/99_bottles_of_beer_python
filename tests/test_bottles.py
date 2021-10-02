b22e1a69ef610d69e2202ef70cb54278d7578bf5
Seungju Han <wade3han@gmail.com>
2021-10-02 15:13:39 +0900

    Make flake8 happy

--- tests/test_bottles.py
+++ tests/test_bottles.py
@@ -3,33 +3,33 @@ import pytest
 from bottles import Bottles
 
 VERSE = {
-    0: "No more bottles of beer on the wall, " + \
-       "no more bottles of beer.\n" + \
-       "Go to the store and buy some more, " + \
+    0: "No more bottles of beer on the wall, " +
+       "no more bottles of beer.\n" +
+       "Go to the store and buy some more, " +
        "99 bottles of beer on the wall.\n",
-    1: "1 bottle of beer on the wall, " + \
-       "1 bottle of beer.\n" + \
-       "Take it down and pass it around, " + \
+    1: "1 bottle of beer on the wall, " +
+       "1 bottle of beer.\n" +
+       "Take it down and pass it around, " +
        "no more bottles of beer on the wall.\n",
-    2: "2 bottles of beer on the wall, " + \
-       "2 bottles of beer.\n" + \
-       "Take one down and pass it around, " + \
+    2: "2 bottles of beer on the wall, " +
+       "2 bottles of beer.\n" +
+       "Take one down and pass it around, " +
        "1 bottle of beer on the wall.\n",
-    3: "3 bottles of beer on the wall, " + \
-       "3 bottles of beer.\n" + \
-       "Take one down and pass it around, " + \
+    3: "3 bottles of beer on the wall, " +
+       "3 bottles of beer.\n" +
+       "Take one down and pass it around, " +
        "2 bottles of beer on the wall.\n",
-    4: "4 bottles of beer on the wall, " + \
-       "4 bottles of beer.\n" + \
-       "Take one down and pass it around, " + \
+    4: "4 bottles of beer on the wall, " +
+       "4 bottles of beer.\n" +
+       "Take one down and pass it around, " +
        "3 bottles of beer on the wall.\n",
-    10: "10 bottles of beer on the wall, " + \
-        "10 bottles of beer.\n" + \
-        "Take one down and pass it around, " + \
+    10: "10 bottles of beer on the wall, " +
+        "10 bottles of beer.\n" +
+        "Take one down and pass it around, " +
         "9 bottles of beer on the wall.\n",
-    99: "99 bottles of beer on the wall, " + \
-        "99 bottles of beer.\n" + \
-        "Take one down and pass it around, " + \
+    99: "99 bottles of beer on the wall, " +
+        "99 bottles of beer.\n" +
+        "Take one down and pass it around, " +
         "98 bottles of beer on the wall.\n",
 }
 
