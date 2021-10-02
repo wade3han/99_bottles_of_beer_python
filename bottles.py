2513eb5bd4039c6dd5419af9bc83d944ee59818b
Seungju Han <wade3han@gmail.com>
2021-10-02 15:37:07 +0900

    Make function out of class

--- bottles.py
+++ bottles.py
@@ -1,44 +1,47 @@
+def unit(number: int) -> str:
+    if number == 0:
+        return "no more"
+    elif number == -1:
+        return "99"
+    else:
+        return str(number)
+
+
+def successor(number: int) -> int:
+    if number == 0:
+        return 99
+    else:
+        return number - 1
+
+
+def container(number: int) -> str:
+    if number == 1:
+        return "bottle"
+    else:
+        return "bottles"
+
+
+def action(number: int) -> str:
+    if number == 0:
+        return "Go to the store and buy some more, "
+    elif number == 1:
+        return "Take it down and pass it around, "
+    else:
+        return "Take one down and pass it around, "
+
+
 class Bottles(object):
     def __init__(self):
         pass
-
-    def unit(self, number: int) -> str:
-        if number == 0:
-            return "no more"
-        elif number == -1:
-            return "99"
-        else:
-            return str(number)
-
-    def successor(self, number: int) -> int:
-        if number == 0:
-            return 99
-        else:
-            return number - 1
-
-    def container(self, number: int) -> str:
-        if number == 1:
-            return "bottle"
-        else:
-            return "bottles"
-
-    def action(self, number: int) -> str:
-        if number == 0:
-            return "Go to the store and buy some more, "
-        elif number == 1:
-            return "Take it down and pass it around, "
-        else:
-            return "Take one down and pass it around, "
 
     def verse(self, number: int) -> str:
         if number < 0 or number > 99:
             raise IndexError("The verse number N should be 0 <= N <= 99.")
         else:
-            return f"{self.unit(number).capitalize()} {self.container(number)} of beer on the wall, " + \
-                   f"{self.unit(number)} {self.container(number)} of beer.\n" + \
-                   f"{self.action(number)}" + \
-                   f"{self.unit(self.successor(number))} {self.container(self.successor(number))} " + \
-                   "of beer on the wall.\n"
+            return f"{unit(number).capitalize()} {container(number)} of beer on the wall, " + \
+                   f"{unit(number)} {container(number)} of beer.\n" + \
+                   f"{action(number)}" + \
+                   f"{unit(successor(number))} {container(successor(number))} of beer on the wall.\n"
 
     def song(self) -> str:
         return self.verses(99, 0)
