7b09fe006caffdd4a69a7c19e6fc2e51a79f0298
Seungju Han <wade3han@gmail.com>
2021-10-02 15:16:18 +0900

    Merge conditional branch elif 2 and else

--- bottles.py
+++ bottles.py
@@ -2,6 +2,12 @@ class Bottles(object):
     def __init__(self):
         pass
 
+    def container(self, number: int) -> str:
+        if number == 1:
+            return "bottle"
+        else:
+            return "bottles"
+
     def verse(self, number: int) -> str:
         if number < 0 or number > 99:
             raise IndexError("The verse number N should be 0 <= N <= 99.")
@@ -15,16 +21,11 @@ class Bottles(object):
                    "1 bottle of beer.\n" + \
                    "Take it down and pass it around, " + \
                    "no more bottles of beer on the wall.\n"
-        elif number == 2:
-            return "2 bottles of beer on the wall, " + \
-                   "2 bottles of beer.\n" + \
-                   "Take one down and pass it around, " + \
-                   "1 bottle of beer on the wall.\n"
         else:
             return f"{number} bottles of beer on the wall, " + \
                    f"{number} bottles of beer.\n" + \
                    "Take one down and pass it around, " + \
-                   f"{number - 1} bottles of beer on the wall.\n"
+                   f"{number - 1} {self.container(number - 1)} of beer on the wall.\n"
 
     def song(self) -> str:
         return self.verses(99, 0)
